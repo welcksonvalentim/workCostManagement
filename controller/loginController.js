a7e3d8d128bdb20df979abaded3f7d4de938dd30
WelcksonValentim-Turma15-TriboB <wellvalentim@hotmail.com>
2022-08-03 10:02:25 -0300

    feat:loginController, cria lógica para permitir o login de pessoas cadastradas

---
+++ controller/loginController.js
@@ -0,0 +1,20 @@
+const { createToken } = require('../utilis/createToken');
+
+const { User } = require('../models');
+
+const loginUser = async (req, res) => {
+  try {
+    const { email } = req.body;
+
+    const user = await User.findOne({ where: { email } });
+    if (!user) return res.status(400).json({ message: 'Invalid fields' });
+
+    const token = createToken(user);
+
+    return res.status(200).json({ token });
+  } catch (error) {
+    return res.status(500).json({ message: 'Erro interno', error });
+  }
+};
+
+module.exports = { loginUser };
