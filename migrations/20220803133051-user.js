4569bfd66facc60962059688fa74a14942613688
WelcksonValentim-Turma15-TriboB <wellvalentim@hotmail.com>
2022-08-03 10:32:44 -0300

    feat:user-migration, cria migration para permitir o login de pessoas cadastradas

---
+++ migrations/20220803133051-user.js
@@ -0,0 +1,27 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('User', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      displayName: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      password: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+    });
+  },
+  async down(queryInterface) {
+    await queryInterface.dropTable('User');
+  },
+};
